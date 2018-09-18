f812b9138d54348d9d7637dc027933e5d957b99f
Adrian <donayo71@gmail.com>
2018-09-18 12:50:08 +0200

    +popup

--- docs/js/script.js
+++ docs/js/script.js
@@ -53,7 +53,22 @@ $(document).ready(function() {
     });
 });
 
+//POPUP BUTTON
+$(document).ready(function () {
+    $('#btntst').click(function (e) {
+        $('.popup-wrap').fadeIn(500);
+        $('.popup-box').removeClass('transform-out').addClass('transform-in');
 
+        e.preventDefault();
+    });
+
+    $('.popup-close').click(function (e) {
+        $('.popup-wrap').fadeOut(500);
+        $('.popup-box').removeClass('transform-in').addClass('transform-out');
+
+        e.preventDefault();
+    });
+});
 
 
  // MENU SECTION ACTIVE
